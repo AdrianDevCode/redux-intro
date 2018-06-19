382cc4a949269334f0753f4178a78fc0f7613f2f
Piosuke <Piosuke@Piosukes-MacBook-Pro.local>
2018-06-19 18:29:11 -0400

    finished ex1,2,3

--- public/exercise4.js
+++ public/exercise4.js
@@ -8,8 +8,16 @@ let defaultTodos = {
 	todos: []
 }
 
+
 // write a reducer that updated the todos array whenever a ADD_TODO action is dispatched
-let reducer4 = (state = defaultTodos, action) => defaultTodos;
+let reducer4 = (state = defaultTodos, action) =>{
+	if(action.type == "ADD_TODO"){
+	
+		return defaultTodos.todos.push(action.todoText);
+	
+	}
+	defaultTodos;
+} 
 
 let store4 = Redux.createStore(reducer4);
 
